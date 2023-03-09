76f057ea699b2c68bb14f5e53956c174dc2687ec
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-03-09 16:17:50 +0530

    error

--- PrimeNumber/src/PrimeNumber.java
+++ PrimeNumber/src/PrimeNumber.java
@@ -20,5 +20,17 @@ public class PrimeNumber {
 		}
 	}
 	
-	
+	public static  isPrime(int n)
+	{
+		int count=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+				count++;
+		}
+		if(count==2)
+			return true;
+		
+		return false;
+	}
 }
